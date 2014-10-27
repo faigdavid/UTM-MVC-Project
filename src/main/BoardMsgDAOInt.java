a66cff1bc25a01b24b674a4fe1003ee79d69f7ea
shk0307 <shaun.kim@mail.utoronto.ca>
2014-10-27 18:16:47 -0400

    deleted BoardMsgDAO

--- src/main/BoardMsgDAOInt.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.util.Iterator;
-
-public interface BoardMsgDAOInt {
-	public void storeMessage(Message message);
-	public void removeMessage(Message message);
-	public Iterator<Message> getMessages();
-}
