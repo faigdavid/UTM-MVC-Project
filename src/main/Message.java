d9010a1ade529e060fc5b6c5d7fe3291975214d5
faigdavi <davidm.faig@gmail.com>
2014-10-28 15:06:10 -0400

    Completed more methods for Message and MessageLocalDAO

--- src/main/Message.java
+++ src/main/Message.java
@@ -4,23 +4,28 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Message {
+	private String mId;
 	private String username;
 	private String boardId;
 	private String text;
 	private String date;
 	private ArrayList<String> whitelist;
 	
+	/*IMPORTANT: The builder cannot make a date or mId, since those
+	 are pre-determined for each message.*/
 	private Message(Builder builder){
+		this.mId = builder.mId;
+		this.date = builder.date;
 		this.username = builder.username;
 		this.boardId = builder.boardId;
 		this.text = builder.text;
-		this.date = builder.date;
 		this.whitelist = builder.whitelist;
 	}
 	
-	public String getMessage(User user) {
+	public String printMessage(User user) {
 		String msg = null;
-		msg = String.format("[%s][%s][%s][%s]", boardId, username, date, text);
+		msg = String.format("[%s][%s][%s][%s][%s]",
+				mId, boardId, username, date, text);
 		
 		if(whitelist == null){ //No whitelist == public message.
 			return msg;
@@ -33,7 +38,9 @@ public class Message {
 		}
 		return "You do not have permission to see this message.";
 	}
-	
+	public String getmId(){
+		return mId;
+	}
 	public String getusername() {
 		return username;
 	}
@@ -56,12 +63,17 @@ public class Message {
 
 	//make sure to check variable names with actual implementation
 	public static class Builder{
+		private String mId;
 		private String username;
 		private String boardId;
 		private String text;
 		private String date;
 		private ArrayList<String> whitelist;
 		
+		public Builder setmId(String mid){
+			this.mId = mid;
+			return this;
+		}
 		public Builder setusername(String username){
 			this.username = username;
 			return this;
@@ -76,12 +88,11 @@ public class Message {
 			this.text = text;
 			return this;
 		}
-		//This will get the exact date the message was made.
-		public Builder setDate(){
-			Date msgdate = new Date();
-			this.date = msgdate.toString();
+		public Builder setDate(String date){
+			this.text = date;
 			return this;
 		}
+		
 		public Builder setWhitelist(ArrayList<String> list){
 			this.whitelist = list;
 			return this;
--- src/main/MessageLocalDAO.java
+++ src/main/MessageLocalDAO.java
@@ -1,7 +1,9 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -9,7 +11,15 @@ import java.sql.Timestamp;
 import java.util.Iterator;
 
 public class MessageLocalDAO implements MessageDAO{
-
+	/*Note: Messages will be stored by mId, board, username, date, and text,
+	 separated by newlines "\n" */
+	
+	/*IMPORTANT!! This class was made for windows file-systems.
+	 * If you wish to change it for linux, then change the path names in
+	 the  methods to linuxpath.*/
+	private String linuxpath = "/CSC301/Messages";
+	private String windowspath = "Libraries\\Documents\\Messages";
+	
 	@Override
 	public Iterator<Message> getMessages(Board board) {
 		
@@ -19,8 +29,40 @@ public class MessageLocalDAO implements MessageDAO{
 
 	@Override
 	public Message getMessage(String mid) {
-		// TODO Auto-generated method stub
-		return null;
+		String filename = String.format("%s/%s.txt", windowspath, mid);
+		BufferedReader reader = null;
+		Message.Builder msgbuild = new Message.Builder();
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+			String line = null;
+		    for (int i=1;i<6;i++) {
+		    	line = reader.readLine();
+		    	if (line == null){
+		    		//Corrupted message.
+		    		return null;
+		    	}
+		    	else if (i == 1){
+		    		msgbuild.setmId(line);
+		    	}
+		    	else if (i == 2){
+		    		msgbuild.setBoardId(line);
+		    	}
+		    	else if (i == 3){
+		    		msgbuild.setusername(line);
+		    	}
+		    	else if (i == 4){
+		    		msgbuild.setDate(line);
+		    	}
+		    	else if (i == 5){
+		    		msgbuild.setText(line);
+		    	}
+		    }
+		} catch (IOException ex) {
+		  // report
+		} finally {
+		   try {reader.close();} catch (Exception ex) {}
+		}
+		return msgbuild.build();
 	}
 
 	@Override
@@ -37,14 +79,16 @@ public class MessageLocalDAO implements MessageDAO{
 
 	@Override
 	public int saveMessage(Board board, Message msg) {
+	String filename = String.format("%s/%s.txt", windowspath, msg.getmId());
 	Writer writer = null;
 	try {
 		writer = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream("filename.txt"), "utf-8"));
-		writer.write(msg.getboardId());
-		writer.write(msg.getusername());
-		writer.write(msg.getDate());
-		writer.write(msg.getText());
+				new FileOutputStream(filename), "utf-8"));
+		writer.write(String.format("%d \n", msg.getmId()));
+		writer.write(String.format("%s \n", msg.getboardId()));
+		writer.write(String.format("%s \n", msg.getusername()));
+		writer.write(String.format("%s \n", msg.getDate()));
+		writer.write(String.format("%s \n", msg.getText()));
 	} catch (IOException e) {
 		e.printStackTrace();
 	} finally {
