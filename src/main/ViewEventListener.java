fd53ab731818a1e8c376471093feb1250228a138
faigdavid <david.faig@mail.utoronto.ca>
2014-11-14 18:47:12 -0500

    Updated functions

--- src/main/ViewEventListener.java
+++ src/main/ViewEventListener.java
@@ -6,10 +6,13 @@ public interface ViewEventListener {
 	public void login(String username, String password);
 	public void register(String userName, String password1,String password2);
 	public void post(String message);
+	public void createBoard(String name);
 	public void requestBoardMessages();
 	public void requestBoards();
 	public void requestSubscribedBoards();
 	//Carlito added these abstract methods()
 	public void subscribeUserToBoard(Board board);
 	public void unsubscribeUserFromBoard(Board board);
+	public void addModelEventListener(ModelEventListener e);
+	public void removeModelEventListener(ModelEventListener e);
 }
