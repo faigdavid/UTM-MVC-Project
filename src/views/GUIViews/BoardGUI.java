77f1c208b29c2ed076608aaadef8d9e265c7519c
R-ed <carlito.llena@gmail.com>
2014-11-20 15:50:34 -0500

    Finished designs for (does not to event handling yet): -DashBoardGUI -BoardGUI
    Side Note: 3 errors in GUIMain, did not fixed them

--- src/views/GUIViews/BoardGUI.java
+++ src/views/GUIViews/BoardGUI.java
@@ -1,100 +1,72 @@
 package GUIViews;
 
-import interfaces.BoardDAOInterface;
-
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Iterator;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
 import javax.swing.JTextField;
 
-import local.BoardDAO;
-import model.*;
-import mvc.ViewEventListener;
 
-public class BoardGUI extends JFrame implements ActionListener , GUIEventListener{
-	
-	
-	private GUIMain GUIMain = null;
-	private ViewEventListener controller = null;
-	
-	private JPanel pane = new JPanel();
+public class BoardGUI extends JFrame implements ActionListener, GUIEventListener {
+
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	private JComboBox subscribedBoards = new JComboBox(); //ComboBox is the drop down menu
-	private JComboBox notSubscribedBoards = new JComboBox();
-	
-	private JTextField board = new JTextField(); //this text field shows the board.
-	private JTextField message = new JTextField();
-	
-	private JButton subscribeButton = new JButton("Subscribe");
-	private JButton unsubscribeButton = new JButton("Unsubscribe");
-	private JButton postButton = new JButton("Post");
-	
+	private JPanel contentPane;
+	private JTextField userInput;
+
+	/**
+	 * Create the frame.
+	 */
 	public BoardGUI(GUIMain listener) {
 		super("4chinz.gov");
-		this.GUIMain = listener;
-		this.controller = GUIMain.getController();
-		
-		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.add(pane);
-		setSize(640, 480); //640x480 Pixels
-		pane.setLayout(new FlowLayout());
+		setBounds(100, 100, 485, 372);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		//added entities to panel, just set the bounds/size, etc. to whatever you wish.
-		pane.add(subscribedBoards);
-		pane.add(notSubscribedBoards);
-		pane.add(board);
-		pane.add(subscribeButton);
-		pane.add(unsubscribeButton);
-		pane.add(postButton);
+		JTextPane board = new JTextPane();
+		board.setEditable(false);
+		board.setBounds(35, 36, 397, 214);
+		contentPane.add(board);
+		
+		JLabel boardTitle = new JLabel("<Insert Board Name Here>");
+		boardTitle.setBounds(35, 11, 179, 14);
+		contentPane.add(boardTitle);
+		
+		userInput = new JTextField();
+		userInput.setBounds(35, 269, 397, 20);
+		contentPane.add(userInput);
+		userInput.setColumns(10);
+		
+		JButton postButton = new JButton("Post");
+		postButton.setBounds(35, 300, 89, 23);
+		contentPane.add(postButton);
 	}
 
-	
 	@Override
-	public void actionPerformed(ActionEvent event) {
-		String selectedBoard = null;
-		
-		if(event.getSource() == subscribeButton) {
-			BoardDAOInterface dao = new BoardDAO();
-			controller.subscribeUserToBoard(dao.getBoardByName(selectedBoard));			
-		}
-		else if(event.getSource() == unsubscribeButton) {
-			BoardDAOInterface dao = new BoardDAO();
-			controller.unsubscribeUserFromBoard(dao.getBoardByName(selectedBoard));			
-		}
-		else if(event.getSource() == postButton) {
-			controller.post(message.getText());
-		}
-		else if(event.getSource() == subscribedBoards ) {
-			selectedBoard = (String) subscribedBoards.getSelectedItem();
-		}
-		else if(event.getSource() == notSubscribedBoards) {
-			selectedBoard = (String) notSubscribedBoards.getSelectedItem();
-		}
-		
+	public int closeGUI() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
-
-	public void recieveMessages(Iterator<Message> messages){
-		//recieve and display the messages.
-	}
-
 
 	@Override
 	public int displayString(String text) {
-		//RETURN 1 FOR SUCCESS!!!!
+		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 	@Override
-	public int closeGUI() {
-		return JFrame.EXIT_ON_CLOSE;
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
-	
 }
--- src/views/GUIViews/DashBoardGUI.java
+++ src/views/GUIViews/DashBoardGUI.java
@@ -2,47 +2,75 @@ package GUIViews;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.util.Iterator;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
-import model.*;
-import mvc.ViewEventListener;
 
-public class DashBoardGUI implements ActionListener, GUIEventListener{
-	
-	private GUIMain GUIMain = null;
-	private ViewEventListener controller = null;
-	
-	public DashBoardGUI(GUIMain listener) { //being in dash board implies we're logged in.
-		GUIMain = listener;
-		controller = GUIMain.getController();
+public class DashBoardGUI extends JFrame implements ActionListener, GUIEventListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public DashBoardGUI(GUIMain Listener) {
+		super("4chinz.gov");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 378, 238);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setBounds(10, 60, 198, 20);
+		contentPane.add(comboBox);
+		
+		JComboBox<String> comboBox_1 = new JComboBox<String>();
+		comboBox_1.setBounds(10, 146, 198, 28);
+		contentPane.add(comboBox_1);
+		
+		JButton btnUnsubscribe = new JButton("Unsubscribe");
+		btnUnsubscribe.setBounds(236, 60, 121, 20);
+		contentPane.add(btnUnsubscribe);
+		
+		JButton btnSusbscribe = new JButton("Susbscribe");
+		btnSusbscribe.setBounds(236, 149, 121, 23);
+		contentPane.add(btnSusbscribe);
+		
+		JLabel lblSusbscribedBoards = new JLabel("Susbscribed Boards");
+		lblSusbscribedBoards.setBounds(10, 17, 163, 32);
+		contentPane.add(lblSusbscribedBoards);
+		
+		JLabel lblBoards = new JLabel("Boards");
+		lblBoards.setBounds(10, 121, 46, 14);
+		contentPane.add(lblBoards);
 	}
 
+	@Override
+	public int closeGUI() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
+	@Override
+	public int displayString(String text) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	public void recieveBoards(Iterator<Board> boards) {
-		//Display the boards.
-		
-	}
-	
-	@Override
-	public int displayString(String text) {
-		//RETURN 1 FOR SUCCESS!!!!
-		return 0;
-	}
-	
-	@Override
-	public int closeGUI() {
-		return JFrame.EXIT_ON_CLOSE;
-		
-	}
-
 }
