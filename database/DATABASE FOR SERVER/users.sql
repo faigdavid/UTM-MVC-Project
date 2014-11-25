086d2bf0283efef88480e4995e1e1bf75be50bf3
shk0307 <shaun.kim@mail.utoronto.ca>
2014-11-25 17:29:51 -0500

    New rules on SQL query

--- database/DATABASE FOR SERVER/users.sql
+++ database/DATABASE FOR SERVER/users.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS subscriptions CASCADE;
-DROP TABLE IF EXISTS messages CASCADE;
-DROP TABLE IF EXISTS boards CASCADE;
-DROP TABLE IF EXISTS tags;
-DROP TABLE IF EXISTS user_permissions;
+DROP TABLE users CASCADE;
+DROP TABLE subscriptions CASCADE;
+DROP TABLE messages CASCADE;
+DROP TABLE boards CASCADE;
+DROP TABLE tags;
+DROP TABLE user_permissions;
 CREATE TABLE users (
     username VARCHAR(30) PRIMARY KEY,
-    passwd  VARCHAR(30)
+    passwd  VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE boards(
     bid SERIAL PRIMARY KEY,
-    name VARCHAR(127),
+    name VARCHAR(127) NOT NULL,
     motd TEXT,
     passwd VARCHAR(30) default NULL,
     visible int default 1
@@ -19,30 +19,30 @@ CREATE TABLE boards(
 );
 
 CREATE TABLE subscriptions (
-    username VARCHAR(30) references users(username) ON DELETE CASCADE,
-    bid INT references boards(bid) ON DELETE CASCADE,
+    username VARCHAR(30) REFERENCES users(username) ON DELETE CASCADE,
+    bid INT REFERENCES boards(bid) ON DELETE CASCADE,
     type INT default 1,
     PRIMARY KEY (bid, username)
 );	
 
 
 CREATE TABLE tags (
-    bid INT references boards(bid) ON DELETE CASCADE,
+    bid INT REFERENCES boards(bid) ON DELETE CASCADE,
     tag VARCHAR(30),
     PRIMARY KEY (bid, tag)
 );
     
 CREATE TABLE messages (
     mid SERIAL PRIMARY KEY,
-    bid INT references boards(bid) ON DELETE CASCADE,    
+    bid INT REFERENCES boards(bid) ON DELETE CASCADE,    
     date_posted TIMESTAMP,
-    username VARCHAR(30) references users(username),
+    username VARCHAR(30) REFERENCES users(username),
     visibility INT default 1,    
     contents TEXT
 );
 
 CREATE TABLE user_permissions(
-    username VARCHAR(30) references users(username) ON DELETE CASCADE PRIMARY KEY,
+    username VARCHAR(30) REFERENCES users(username) ON DELETE CASCADE PRIMARY KEY,
     permission INT default 0
 );
 
